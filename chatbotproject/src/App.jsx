e2a304a629acbf40701aa11d4c7058d248069a30
Luke Wang <lukewang74@gmail.com>
2025-09-20 23:27:12 -0700

    Add basic interface

--- chatbotproject/src/App.jsx
+++ chatbotproject/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useRef } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const tokenInput = useRef(null)
+
+  function handleShopifyToken() {
+    alert(tokenInput.current.value);
+  }
 
   return (
     <>
       <div>Shopify API Test</div>
-      
+      <p>Enter Shopify API Token:</p>
+      <input ref={tokenInput}></input>
+      <button onClick={() => handleShopifyToken()}>Submit</button>
     </>
   )
 }
